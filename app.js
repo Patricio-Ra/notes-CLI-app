55c995cde2352d2dc69b0c56bd9c44d416158b16
Patricio R <patricio.raschetti@gmail.com>
2019-12-07 20:26:22 -0300

    Add validator, chalk and nodemon npm modules

---
+++ app.js
@@ -0,0 +1,8 @@
+const validator = require('validator');
+const chalk = require('chalk');
+const getNotes = require('./notes.js');
+
+const msg = getNotes();
+
+console.log(msg);
+console.log(chalk.blue.bold('Success!'));
