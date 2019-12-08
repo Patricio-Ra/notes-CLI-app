412068bd4c67aea4d302aeb6933561fecc83dba0
Patricio R <patricio.raschetti@gmail.com>
2019-12-08 01:15:27 -0300

    Add Yargs module and basic args config.

--- app.js
+++ app.js
@@ -1,8 +1,44 @@
-const validator = require('validator');
 const chalk = require('chalk');
+const yargs = require('yargs');
 const getNotes = require('./notes.js');
 
-const msg = getNotes();
+// Customize version.
+yargs.version('1.1.0');
 
-console.log(msg);
-console.log(chalk.blue.bold('Success!'));
+// Add command.
+yargs.command({
+    command: 'add',
+    describe: 'Adds a new note',
+    handler: () => {
+        console.log(chalk.green.bold('Adding a new note.'));
+    }
+});
+
+// Remove command.
+yargs.command({
+    command: 'remove',
+    describe: 'Removes a new note',
+    handler: () => {
+        console.log(chalk.red.bold('Removing a new note.'));
+    }
+});
+
+// List command.
+yargs.command({
+    command: 'list',
+    describe: 'Lists all the notes',
+    handler: () => {
+        console.log(chalk.blue.bold('Listing out all the notes.'));
+    }
+});
+
+// Read command.
+yargs.command({
+    command: 'read',
+    describe: 'Reads the selected note',
+    handler: () => {
+        console.log(chalk.blue.bold('Reading the selected note.'));
+    }
+});
+
+console.log(yargs.argv);
